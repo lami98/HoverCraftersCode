2acdb728675ffa87f23a717fcd0c97327161aaf0
Leif Magnussen <leif.magnussen2@gmail.com>
2018-01-28 16:20:14 -0800

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -2,7 +2,7 @@
 
  Servo servoOne
  int servoOnePin = 0;
- int servoOneVal;
+ int servoOneVal = 0;
  int servoOnePosition = 0;
 void setup() {
 servoOne.attach(9);
@@ -13,7 +13,7 @@ void loop() {
 	// this will be where the functions are called when the buttons on the remote are used	
   hoveringMotors();
   droppingMechanism();
-  backMotorAndServo();
+  backMotorAndServo(servoOneVal);
 
 }
 
@@ -26,10 +26,10 @@ void droppingMechanism() {
 	// this function is to control the dropping mechanism
 }
 
-void backMotorAndServo() {
+void backMotorAndServo(servoOneVal) {
 	// this function is to control the motor that makes the craft move forward and the servo that 
 	// moves that motor
-	servoOneVal = analogRead(servoOnePin);
+	
   	servoOnePosition = map(servoOneVal, 0, 1023, 0, 180);
   	servoOne.write(servoOnePosition);
 
